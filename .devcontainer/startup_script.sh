91921d739dd53ffc3166ef440b9d5ad712510ffd
Sam Gabrail <sam.gabrail@tekanaid.com>
2024-07-25 14:28:37 -0400

    update with no interactive akeyless cli

--- .devcontainer/startup_script.sh
+++ .devcontainer/startup_script.sh
@@ -3,5 +3,5 @@ cat <<EOL > ~/Instructions.txt
 This is the URL to the AWS Console.
 https://047709130171.signin.aws.amazon.com/console/
 EOL
-
+akeyless --init
 akeyless configure --profile default --access-id ${AKEYLESS_ACCESS_ID} --access-key ${AKEYLESS_ACCESS_KEY}
