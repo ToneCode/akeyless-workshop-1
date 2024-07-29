7e3caf1ff4aee1b659d4fb30301458f600779dc0
Sam Gabrail <sam.gabrail@tekanaid.com>
2024-07-29 17:26:08 -0400

    kms fix?

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,10 +24,10 @@ module "eks" {
   cluster_name    = local.cluster_name
   cluster_version = var.cluster_version
 
-  vpc_id     = data.terraform_remote_state.vpc.outputs.vpc_id
-  subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
+  vpc_id                         = data.terraform_remote_state.vpc.outputs.vpc_id
+  subnet_ids                     = data.terraform_remote_state.vpc.outputs.private_subnets
   cluster_endpoint_public_access = true
-
+  kms_key_enable_default_policy  = true
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
   }
